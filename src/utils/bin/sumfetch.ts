5997a7171db8f00278dc18c0676d2e452fba08c5
Lok/Tom Chow <lohchness@gmail.com>
2023-12-07 14:51:58 +0800

    update links, ascii art

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -11,10 +11,10 @@ const sumfetch = async (args: string[]): Promise<string> => {
  @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
 @@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 @@                                   @@     -----------
-@@             .@@@@@@@@@@.          @@      CONTACT 
- @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+@@                                   @@      CONTACT 
+ @@         @@     @@     @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  @@         @@   @@ @@  @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   @@          @@@     @@         @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
      @@@                        @@@         -----------
         @@@                  @@@ @@         OTHERS 
          @|  @@@@@@@@@@@@@@@@   @@          <u><a href="${config.other_urls.spotify}" target="_blank">${config.other_urls.spotify}</a></u>
@@ -30,7 +30,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
    ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
 ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 ▐▓                                 ▐▓       -----------
-▐▓       > L O H C H N E S S       ▐▓        CONTACT 
+▐▓      > L O H C H N E S S        ▐▓        CONTACT 
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
